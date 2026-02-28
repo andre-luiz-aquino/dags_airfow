528d55f545852d9dfb96a783538c6f135dea9a67
andre-luiz-aquino <andre.luiz.aquino30@gmail.com>
2026-02-28 20:03:37 -0300

    dag bitcoin

---
+++ dags/integracao_btc.py
@@ -0,0 +1,44 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from airflow.models import Variable
+
+
+# Definição padrão de argumentos das tasks
+default_args = {
+    "owner": "airflow",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id="rodar_imagem_dockerhub",
+    description="DAG que executa um container com imagem do Docker Hub",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval="*/5 * * * *",  # roda a cada 5 minutos
+    catchup=False,
+    default_args=default_args,
+    tags=["docker", "kubernetes"],
+) as dag:
+
+
+    # Recupera a credencial da variável do Airflow
+
+    db_dsn = Variable.get("DB_DSN")
+
+    executar_container = KubernetesPodOperator(
+        task_id="executar_imagem_dockerhub",
+        name="executar-imagem-dockerhub",
+        namespace="airflow",  # ajuste se seu namespace for outro
+        image="andreaquino/integracao_btc:latest",  # imagem correta
+        image_pull_policy="Always",
+        get_logs=True,
+        is_delete_operator_pod=True,  # remove o pod após terminar
+        env_vars={
+            "DB_DSN": db_dsn,
+        },  # passa apenas DB_DSN como variável de ambiente
+    )
+
+    executar_container
